87ea4efdc01d75b595e1a98c67bae9cc4fce80c9
Sandeep Dhamala <141273955+sandeep0600@users.noreply.github.com>
2024-05-26 23:02:53 +0545

    Update HeroSlide.jsx

--- src/components/hero-slide/HeroSlide.jsx
+++ src/components/hero-slide/HeroSlide.jsx
@@ -105,7 +105,7 @@ const HeroSlideItem = ({ item, className, openEmbedModal }) => {
     }
 
     const handleWatchNow = () => {
-        const embedUrl = `https://vidsrc.pro/embed/movie?tmdb=${item.id}`;
+        const embedUrl = `https://vidsrc.to/embed/movie?tmdb=${item.id}`;
         openEmbedModal(embedUrl);
     }
 

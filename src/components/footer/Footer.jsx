dd14d84d56a97252c248314920a9f053c1621d18
Sandeep Dhamala <141273955+sandeep0600@users.noreply.github.com>
2024-05-27 01:32:22 +0545

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import './footer.scss';
 import { Link } from 'react-router-dom';
 
 import bg from '../../assets/footer-bg.jpg';
-import logo from '../../assets/tmovie.png';
+import logo from '../../assets/m.png';
 
 const Footer = () => {
     return (
@@ -13,28 +13,13 @@ const Footer = () => {
             <div className="footer__content container">
                 <div className="footer__content__logo">
                     <div className="logo">
-                        <img src={logo} alt="" />
-                        <Link to="/">tMovies</Link>
+                        <img src={logo} alt="NEPX Logo" />
+                        <Link to="/">NEPX</Link>
                     </div>
                 </div>
-                <div className="footer__content__menus">
-                    <div className="footer__content__menu">
-                        <Link to="/">Home</Link>
-                        <Link to="/">Contact us</Link>
-                        <Link to="/">Term of services</Link>
-                        <Link to="/">About us</Link>
-                    </div>
-                    <div className="footer__content__menu">
-                        <Link to="/">Live</Link>
-                        <Link to="/">FAQ</Link>
-                        <Link to="/">Premium</Link>
-                        <Link to="/">Pravacy policy</Link>
-                    </div>
-                    <div className="footer__content__menu">
-                        <Link to="/">You must watch</Link>
-                        <Link to="/">Recent release</Link>
-                        <Link to="/">Top IMDB</Link>
-                    </div>
+                <div className="footer__content__info">
+                    <p>&copy; {new Date().getFullYear()} NEPX</p>
+                    <p>This website does not host any content. Everything is hosted as an API and found free from other sites. Video URLs are from Vidsrc.</p>
                 </div>
             </div>
         </div>
